be5ec523e8fe9e87daae218d73df5c4c7a03a1bc
James404 <68128416+James404-cyber@users.noreply.github.com>
2021-02-11 14:51:32 +0300

    Create __init__.py

---
+++ main/__init__.py
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade
+$pkg install python
+$ pkg install python2
+$ pkg install git
+$ pip install requests
+$ pip2 install requests
+$ python -m pip install uncompyle6
+$ python2 -m pip install uncompyle6
+$ git clone https://github.com/James404-cyber/Decompile.git
+$ cd Decompile
+$ python2 Compile.py
